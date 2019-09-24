99d07635e0f3379edf9f9b2361e530ef8c311810
killian05000 <killianwolfger@hotmail.fr>
2019-09-24 16:07:23 +0200

    adding an identifier on each object

--- ARO/TP2/parser.py
+++ ARO/TP2/parser.py
@@ -2,12 +2,14 @@ from item import Item
 
 def parser(file_name):
     try:
+        line_number = 1
         with open(file_name, 'r') as filehandler:
             bag_size = filehandler.readline()
             item_list = []
             for line in filehandler:
+                line_number += 1
                 weight, value = line.split(" ")
-                item_list.append(Item(float(weight), float(value)))
+                item_list.append(Item(int(weight), int(value), line_number))
             return bag_size, item_list
     except FileNotFoundError:
         print("An error occured while trying to read the file :", filename)
