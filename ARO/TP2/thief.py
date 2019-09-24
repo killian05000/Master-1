ff517f5aa9b56de256d08dd38449bc5f41c04a3e
killian05000 <killianwolfger@hotmail.fr>
2019-09-24 17:21:37 +0200

    recursive branch and bround function

--- ARO/TP2/thief.py
+++ ARO/TP2/thief.py
@@ -23,3 +23,35 @@ class Thief:
             if self.bag.weight >= self.bag.size :
                 break
         print(self.bag)
+
+    def branch_and_bound_fill(self):
+        self.bag.clear()
+        taken_items = []
+        opt, taken_items = self.branch_and_bound(self.item_list, self.bag.size, taken_items)
+        for i in taken_items:
+            self.bag.add(i)
+        print(self.bag)
+
+    def branch_and_bound(self, item_list: List[Item], capacity: int, taken_items: List[Item]) -> float:
+        if(len(item_list) == 0):
+            #print("je sors dans la condition feuille")
+            return sum(i.value for i in taken_items), taken_items
+
+        item = item_list[0]
+        item_list = item_list[1:]
+
+        if(capacity - item.weight < 0):
+            #print("je sors dans la condition bag obèse")
+            return sum(i.value for i in taken_items), taken_items
+
+        sum1, taken_items1 = self.branch_and_bound(item_list, capacity - item.weight, taken_items+[item])
+        sum2, taken_items2 = self.branch_and_bound(item_list, capacity, taken_items)
+
+        #print("sum1 : ", sum1, " | sum2 : ", sum2)
+
+        if(sum1 > sum2):
+            #print("je sors avec SUM1")
+            return sum1, taken_items1
+        else:
+            #print("je sors avec SUM2")
+            return sum2, taken_items2
