98e7e10d389fde03c40d7c8f88e0c4877b4bf499
killian05000 <killianwolfger@hotmail.fr>
2019-09-24 16:08:29 +0200

    adding an identifier

--- ARO/TP2/item.py
+++ ARO/TP2/item.py
@@ -1,17 +1,22 @@
 from __future__ import annotations
 
 class Item:
-    def __init__(self, weight: int, value: int):
+    def __init__(self, weight: int, value: int, identifier: int):
         self.weight = weight
         self.value = value
         self.ratio = float(self.value) / float(self.weight)
+        self.identifier = identifier
 
     def __eq__(self, o: Item) -> boolean:
         return (
                 self.__class__ == o.__class__ and
                 self.weight == o.weight and
-                self.value == self.value
+                self.value == o.value and
+                self.identifier == o.identifier
          )
 
     def __str__(self):
-        return f"weight : {self.weight} | value : {self.value} | ratio : {self.ratio}"
+        return f"(ID#{self.identifier} weight : {self.weight} | value : {self.value} | ratio : {self.ratio})"
+
+    def __repr__(self):
+        return self.__str__()
