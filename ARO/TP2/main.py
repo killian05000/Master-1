326f3d587f4c307352ed9c0d3f2517ccb96f405b
killian05000 <killianwolfger@hotmail.fr>
2019-09-24 16:10:09 +0200

    cleaning the main, distributing the code in other files

--- ARO/TP2/main.py
+++ ARO/TP2/main.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from thief import Thief
 from item import Item
 from bag import Bag
 from parser import *
@@ -10,14 +11,22 @@ if (len(sys.argv) > 1):
 
 
 bag_size, item_list = parser(filename)
-b = Bag(bag_size)
-
-for i in range(len(item_list)):
-    print(item_list[i])
-
 item_list.sort(key=lambda x: x.ratio, reverse=True)
+thief = Thief(Bag(bag_size), item_list)
+'''
+for i in item_list:
+    print(i)
+'''
 
-print("BBBBBBBBBBBBBBBBBB")
+################## Naive sorting ##################
+print("-------------------------- Naive sorting --------------------------")
+thief.naive_fill()
+print("-------------------------- Naive sorting --------------------------\n")
+################## Naive sorting ##################
 
-for i in range(len(item_list)):
-    print(item_list[i])
+
+##################  Greedy sorting ##################
+print("-------------------------- Greedy sorting --------------------------")
+thief.greedy_fill()
+print("-------------------------- Greedy sorting --------------------------\n")
+##################  Greedy sorting ##################
