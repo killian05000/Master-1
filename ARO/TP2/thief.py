905979c75e5d0555dc7c45b749c0bdad416a6a21
killian05000 <killianwolfger@hotmail.fr>
2019-10-01 15:56:07 +0200

    problem found, trying different methods

--- ARO/TP2/thief.py
+++ ARO/TP2/thief.py
@@ -1,6 +1,7 @@
 from typing import *
 from bag import Bag
 from item import Item
+import copy
 
 
 class Thief:
@@ -47,27 +48,37 @@ class Thief:
 
     def branch_and_bound(self, item_list: List[Item], capacity: int, taken_items: List[Item]):
         tmpSum = sum(i.value for i in taken_items)
+
         if(len(item_list) == 0):
             if(self.valeurMax < tmpSum):
                 self.valeurMax = tmpSum
             return tmpSum, taken_items
 
         item = item_list[0]
-        item_list = item_list[1:]
+        #item_list.pop(0) #faster but much smaller result
 
-        if(tmpSum + self.greedy_fill_bab(capacity, item_list)) <= self.valeurMax:
+        '''
+        #a bit faster but smaller result
+        item_list2 = []
+        item_list.pop(0)
+        for i in range(len(item_list)):
+            item_list2.append(item_list[i])
+        '''
+
+        item_list2 = item_list[1:] #result correct but very time consuming
+
+        if(tmpSum + self.greedy_fill_bab(capacity, item_list2)) <= self.valeurMax:
             self.count += 1
             #print("Vmax : ", self.valeurMax, " | ", self.count, "branches trimmed / ", self.nbPossibilities)
             return 0, []
 
-
         # If we got a too heavy object (capacity - item.weight < 0), we idon't take it
         sum1=0
         if(capacity - item.weight >= 0):
-            sum1, taken_items1 = self.branch_and_bound(item_list, capacity - item.weight, taken_items+[item])
+            sum1, taken_items1 = self.branch_and_bound(item_list2, capacity - item.weight, taken_items+[item])
 
         # but we are still gonna check if there is lighter items after it instead of stopping here
-        sum2, taken_items2 = self.branch_and_bound(item_list, capacity, taken_items)
+        sum2, taken_items2 = self.branch_and_bound(item_list2, capacity, taken_items)
 
         if(sum1 > sum2):
             return sum1, taken_items1
